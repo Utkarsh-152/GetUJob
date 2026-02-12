901f20b71753c3694d7ce71ab40340742f594f2c
Utkarsh Tripathi <shreytripathi2004@gmail.com>
2026-02-12 14:17:26 +0530

    frontend error resolved for login jobseeker

--- frontend/GetUJob/src/App.jsx
+++ frontend/GetUJob/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home.jsx';
 import JobSeeker from './pages/JobSeeker.jsx';
 import ReferralGiverRegister from './pages/ReferralGiverRegister.jsx';
 import LoginReferralGivers from './pages/LoginReferralGivers.jsx';
-import LoginJobseeker from './pages/LoginJobseeker.JSX';
+import LoginJobseeker from './pages/LoginJobseeker.jsx';
 
 function App() {
   return (
